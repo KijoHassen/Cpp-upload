ba15cb6262f93a03df1a0244a9d2fcd829f01aae
KijoHassen <g3471861661@gmail.com>
2024-09-24 11:19:46 +0800

    20240924

---
+++ NPU/c.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    printf("hahahahahahah");
+
+    system("pause");
+    return 0;
+}
--- NPU/sqrt.cpp
+++ NPU/sqrt.cpp
@@ -4,11 +4,12 @@
 double root(double x, double y)
 {
     if (x>=y) return sqrt(x-y);
-    else return 0;
+    else printf("Illegal Arguments");
+    return 0;
 }
 int main()
 {
     double a,b;
     scanf("%lf%lf",&a,&b);
-    
+    printf("%lf\n", root(a,b));
 }
