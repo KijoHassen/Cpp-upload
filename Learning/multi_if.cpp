6ed6a07bd1742743f58882d79e335e01a86d5d29
KijoHassen <g3471861661@gmail.com>
2024-09-26 18:30:57 +0800

    20240926

---
+++ Learning/multi_if.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int score;
+    printf("Enter your score:");
+    scanf("%d",&score);
+    printf("Your score is: %d\n",score);
+
+    if(score>600)
+    {
+        
+    }
+    
+    system ("pause");
+    return 0;
+}
