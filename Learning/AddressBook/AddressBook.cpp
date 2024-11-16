3602f567bc4b54cbbcaa5d6fb801754bdfcb95cd
KijoHassen <g3471861661@gmail.com>
2024-11-17 01:09:27 +0800

    Add func "showMenu"

--- Learning/AddressBook/AddressBook.cpp
+++ Learning/AddressBook/AddressBook.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
+//显示菜单界面
+void showMenu()
+{   printf("$$$$$$$$$$$$$$$$$$$$$$$$\n");
+    printf("$$$$$ 1.添加联系人 $$$$$\n");
+    printf("$$$$$ 2.显示联系人 $$$$$\n");
+    printf("$$$$$ 3.删除联系人 $$$$$\n");
+    printf("$$$$$ 4.查询联系人 $$$$$\n");
+    printf("$$$$$ 5.修改联系人 $$$$$\n");
+    printf("$$$$$ 6.清空联系人 $$$$$\n");
+    printf("$$$$$ 0.退出通讯录 $$$$$\n");
+    printf("$$$$$$$$$$$$$$$$$$$$$$$$\n");
+}
+
 int main(){
-    
+    //调用菜单界面
+    showMenu();
     
 
     system ("pause");
