ab658b87550a682661e1eb5a5274b78316059e4f
KijoHassen <g3471861661@gmail.com>
2024-12-03 12:08:52 +0800

    202412031208

--- Learning/_uncategorized/string.cpp
+++ Learning/_uncategorized/string.cpp
@@ -2,13 +2,12 @@
 
 int main(){
     
-    int num[] = {1,2,3,4,5};
     char names[5] = {'m','a','y','e'}; //是字符串
     char names1[5] = {'m','a','y','e','w'}; //不是字符串，没有储存\0
     printf("%s|%s\n", names, names1);
 
     char str[] = "My name is AAA";//是字符串
-    printf("%s", str);
+    printf("%s\n", str);
 
     const char *pstr = "My name is AAA"; //不能修改
 
