3460d385d6b4c49a509c29cfcdd2fe41c0751219
KijoHassen <g3471861661@gmail.com>
2025-02-15 20:25:31 +0800

    123

--- Learning/_uncategorized/string_array.cpp
+++ Learning/_uncategorized/string_array.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-
-int main(){
-    
-    
-
-    system ("pause");
-    return 0;
-}
+blender -b "E:\3D\BlenderPrj\Plot\20241201_SexPub\Workflow\Pub\2980-4100.blend" -E BLENDER_EEVEE_NEXT -o "I:\BlenderOut\P1_2\f\f" -a
+blender -b "E:\3D\BlenderPrj\Plot\20241201_SexPub\Workflow\Pub\4101-4780.blend" -E BLENDER_EEVEE_NEXT -o "I:\BlenderOut\P1_2\g\g" -a
+blender -b "E:\3D\BlenderPrj\Plot\20241201_SexPub\Workflow\Pub\4781-6000.blend" -E BLENDER_EEVEE_NEXT -o "I:\BlenderOut\P1_2\h\h" -a
