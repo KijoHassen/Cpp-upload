c125b60ca633e1478202544573f03fb9b5ba3f36
KijoHassen <g3471861661@gmail.com>
2025-09-30 13:45:28 +0800

    null

--- Exp/1.cpp
+++ Exp/1.cpp
@@ -1,2 +1,12 @@
 #include<iostream>
 
+int main(){
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    int sum = a + b + c + d;
+    double average = sum / 4.0; //4.0浮点结果，4整除
+    printf("Sum = %d; Average = %.1f", sum, average);
+
+    system("pause");
+    return 0;
+}
