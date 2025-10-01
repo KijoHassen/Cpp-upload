69270c16ab39c7ce4df0b305fb1521b02022a891
KijoHassen <g3471861661@gmail.com>
2025-10-02 01:25:02 +0800

    null

--- Exp/1.cpp
+++ Exp/1.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+using namespace std;
+
+#include<stdio.h>
+#include<stdlib.h>
+
 
 int main(){
     int a,b,c,d;
     scanf("%d %d %d %d",&a,&b,&c,&d);
     int sum = a + b + c + d;
     double average = sum / 4.0; //4.0浮点结果，4整除
-    printf("Sum = %d; Average = %.1f", sum, average);
+    printf("Sum = %d; Average = %.1f\n", sum, average);
+    cout << "abc" << sizeof(long double) << endl;
 
     system("pause");
     return 0;
